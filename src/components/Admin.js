e5e09d01a6b2416fb3d304b26145702af72b18d9
eparkhurst <eli.parkhurst@gmail.com>
2016-12-13 11:03:12 -0800

    added POST functionality to Admin Component. There isn't any styling and currently the current location of the user is automatically added and sent in the request body.

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,9 +1,50 @@
 import React , {Component} from 'react'
+import 'whatwg-fetch'
 
 class Admin extends Component{
+  constructor(){
+    super()
+    this.sendData = this.sendData.bind(this)
+    this.getLocation = this.getLocation.bind(this)
+    this.showPosition = this.showPosition.bind(this)
+  }
+  sendData(loc){
+    console.log(loc);
+    fetch('http://localhost:3000/locations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: this.refs.title.value,
+        text: this.refs.text.value,
+        location:loc
+      })
+    })
+  }
+  getLocation(event) {
+    event.preventDefault()
+    if (navigator.geolocation) {
+        return navigator.geolocation.getCurrentPosition(this.showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.")
+    }
+  }
+
+  showPosition(position) {
+    var loc = {lat:position.coords.latitude, lng:position.coords.longitude}
+    this.sendData(loc)
+  }
+
   render(){
     return <div>
-      <h1>From Admin. This shit is Secret</h1>
+      <form onSubmit={this.getLocation}>
+        <label>Title</label>
+        <input type="text" ref="title"/>
+        <label>text</label>
+        <textarea ref="text"/>
+        <button type="submit">Add Blog</button>
+      </form>
     </div>
   }
 }
